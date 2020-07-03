e7122ded5039ad913ea7067cadfce032dcda70bb
mdsabur <mdsabur@gmail.com>
2020-07-03 18:34:01 +0600

    Add Push Notification

--- screen/Details.js
+++ screen/Details.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import * as LocatioAn from 'expo-location';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function Details() {
     const [location, setLocation] = useState(null);
--- screen/TabADetailsScreen.js
+++ screen/TabADetailsScreen.js
@@ -5,6 +5,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 
 export default function TabADetailsScreen({ navigation }) {
+
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>
